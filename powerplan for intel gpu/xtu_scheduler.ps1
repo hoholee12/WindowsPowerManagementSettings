7e5cf6241f3a16615d7bf1c43c1c548ef309269d
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2019-06-08 16:30:22 +0900

    test

--- powerplan for intel gpu/xtu_scheduler.ps1
+++ powerplan for intel gpu/xtu_scheduler.ps1
@@ -1,11 +1,15 @@
 # http://www.bigsoft.co.uk/blog/2013/09/30/change-power-plan-when-application-starts
+# https://devblogs.microsoft.com/scripting/use-powershell-and-wmi-to-get-processor-information/
 # rewritten by manual
 
 # run it via task scheduler 
-#  PowerShell.exe -windowstyle hidden -executionpolicy remotesigned <scriptlocation>\xtu_scheduler.ps1 (Run whether user is logged on or not is VERY UNRELIABLE)
+#  PowerShell.exe -windowstyle hidden -executionpolicy remotesigned <scriptlocation>\xtu_scheduler.ps1
+#  (Run whether user is logged on or not is VERY UNRELIABLE)
 
 #install intel xtu
 #read cpu clock.txt and set up everything prior to using this script
+
+#Config Area Herevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 # throttle[0], maxpower[1], encoding[2]
 # your program = powerplan
@@ -36,11 +40,25 @@ $programs_running_cfg_xtu = @{
 	2 = 4.5		#450mhz
 }
 
-$loop_delay = 5
+# initial xtu setting
+$xtu_init = 7.5		#750mhz		your 'Balanced' gpu setting
+$xtu_max = 10.5		#1050mhz	top speed (even 50mhz off the ogspeed and you will be frying your cpu)
 
-# initial powerplan
-powercfg -setactive '381b4222-f694-41f0-9685-ff5bb260df2e'
-xtucli -t -id 59 -v 7.5
+
+$loop_delay = 5		#seconds
+
+#Config Area Here^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+# initial powerplan 'Balanced'
+powercfg -setactive '381b4222-f694-41f0-9685-ff5bb260df2e'		#'Balanced'
+xtucli -t -id 59 -v $xtu_init
+
+# initial cpu max speed
+$cpu = Get-WmiObject -class Win32_Processor
+$max = $cpu['CurrentClockSpeed']
+
 
 while ($True)
 {
@@ -62,17 +80,27 @@ while ($True)
 		#change power plan
 		if ($current -match $programs_running_cfg_guid[$special_programs[$key]] -eq $False)
 		{
-			powercfg -setactive $programs_running_cfg_guid[$special_programs[$key]]
-			xtucli -t -id 59 -v $programs_running_cfg_xtu[$special_programs[$key]]
+			$cpu = Get-WmiObject -class Win32_Processor
+			$load = $cpu['LoadPercentage']
+			$clock = $cpu['CurrentClockSpeed']
+			#if throttling has kicked in('Balanced' clockspeed must be set lower than 'Performance')
+			if($clock -le $max){
+				powercfg - setactive '8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c'		#'Performance'
+				xtucli -t -id 59 -v $xtu_max
+			}
+			else{
+				powercfg -setactive $programs_running_cfg_guid[$special_programs[$key]]
+				xtucli -t -id 59 -v $programs_running_cfg_xtu[$special_programs[$key]]
+			}
 		}
 	}
 	else
 	{
-		#change back to balanced if nothings running
+		#change back to 'Balanced' if nothings running
 		if ($current -match '381b4222-f694-41f0-9685-ff5bb260df2e' -eq $False)
 		{
-			powercfg -setactive '381b4222-f694-41f0-9685-ff5bb260df2e'
-			xtucli -t -id 59 -v 7.5
+			powercfg -setactive '381b4222-f694-41f0-9685-ff5bb260df2e'		#'Balanced'
+			xtucli -t -id 59 -v $xtu_init
 		}
 
 	}
