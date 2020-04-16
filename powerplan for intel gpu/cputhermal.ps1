9ea559b1b578e279b18e3fdff4828bc1924524db
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-04-17 03:47:46 +0900

    much better way

---
+++ powerplan for intel gpu/cputhermal.ps1
@@ -0,0 +1,15 @@
+#load dll
+[system.reflection.assembly]::loadfile("" + (get-location) + "\openhardwaremonitorlib.dll")
+
+while($true){
+	$ohm = new-object openhardwaremonitor.hardware.computer
+	$ohm.cpuenabled = $true
+	$ohm.open()
+
+	$ohm.hardware
+	$ohm.hardware.sensors
+	$ohm.hardware.sensors.sensortype
+	$ohm.hardware.sensors.value
+	
+	start-sleep 5
+}
